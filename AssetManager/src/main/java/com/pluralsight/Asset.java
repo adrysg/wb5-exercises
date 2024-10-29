7fdaa357d8e08761ea025c6551fbbeabc7031a8b
agabin <agabin@appdev.yearup.org>
2024-10-29 15:57:12 -0400

    Started AssetManager

---
+++ AssetManager/src/main/java/com/pluralsight/Asset.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+public class Asset {
+
+    String description;
+    String dateAcquired;
+    double originalCost;
+
+    public Asset(String description, String dateAcquired, double originalCost) {
+        this.description = description;
+        this.dateAcquired = dateAcquired;
+        this.originalCost = originalCost;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateAcquired() {
+        return dateAcquired;
+    }
+
+    public void setDateAcquired(String dateAcquired) {
+        this.dateAcquired = dateAcquired;
+    }
+
+   public double getValue(){
+        return originalCost;
+   }
+}
