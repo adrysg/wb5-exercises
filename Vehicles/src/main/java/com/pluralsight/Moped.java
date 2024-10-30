7bd0fe80857d4fec619d287b748cde467ef6674b
agabin <agabin@appdev.yearup.org>
2024-10-30 15:34:59 -0400

    Updates

--- Vehicles/src/main/java/com/pluralsight/Moped.java
+++ Vehicles/src/main/java/com/pluralsight/Moped.java
@@ -5,8 +5,11 @@ public class Moped extends Vehicle{
    private String underseatStorage;
 
     public Moped(float speedMax, float speedCurrent, boolean isManual, boolean isElectric, boolean isGasoline,
-                 String vehicleType, int fuelCapacity, int cargoCapacity, int numberOfPassengers, String color) {
+                 String vehicleType, int fuelCapacity, int cargoCapacity, int numberOfPassengers, String color,
+                 String underseatStorage) {
+
         super(speedMax, speedCurrent, isManual, isElectric, isGasoline, vehicleType, fuelCapacity, cargoCapacity, numberOfPassengers, color);
+        this.underseatStorage = underseatStorage;
     }
 
     public String getUnderseatStorage() {
